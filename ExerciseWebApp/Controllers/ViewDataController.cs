c5e21149da4b799e277bc5b26e801c1681d81ec3
Marius Toporascu <mariustoporascu@yahoo.com>
2022-12-18 12:14:50 +0200

    complete excercise

--- ExerciseWebApp/Controllers/ViewDataController.cs
+++ ExerciseWebApp/Controllers/ViewDataController.cs
@@ -8,8 +8,8 @@ using System.Text;
 
 namespace ExerciseWebApp.Controllers
 {
-	public class ViewDataController : Controller
-	{
+    public class ViewDataController : Controller
+    {
         private readonly IMemoryStorage _memoryStorage;
         public ViewDataController(IMemoryStorage memoryStorage)
         {
@@ -17,57 +17,26 @@ namespace ExerciseWebApp.Controllers
         }
         public IActionResult Index() => View(_memoryStorage.GetAll<Company>().Select(company => new CompanyViewModel(company)));
 
-        // For more dynamic sorting i would use a nuget package called System.Linq.Dynamic.Core
-        // But for the purpose of this exercise i will give the posibility to sort by 2 columns only
         [HttpPost("SortData")]
         public IActionResult SortData(string[] sortCriteria)
         {
-            if (sortCriteria.Length == 0) return RedirectToAction("Index");
-
             IEnumerable<CompanyViewModel> companies = _memoryStorage.GetAll<Company>().Select(company => new CompanyViewModel(company));
-            
-            string[] parts1st = sortCriteria[0].Split(' ');
-            string sortColumn1st = parts1st[0];
-            string sortDirection1st = parts1st[1];
-            PropertyInfo propertyInfo1st = typeof(Company).GetProperty(sortColumn1st) ?? typeof(Contact).GetProperty(sortColumn1st);
-            if (sortCriteria.Length == 1)
-            {
-                companies = sortDirection1st == "asc" ? 
-                    companies.OrderBy( c => propertyInfo1st.DeclaringType == typeof(Company) ? 
-                        propertyInfo1st.GetValue(c) : propertyInfo1st.GetValue(c.ContactDetails)) : 
-                    companies.OrderByDescending( c => propertyInfo1st.DeclaringType == typeof(Company) ? 
-                        propertyInfo1st.GetValue(c) : propertyInfo1st.GetValue(c.ContactDetails));
-            }
-            if (sortCriteria.Length == 2)
-            {
-                string[] parts2nd = sortCriteria[1].Split(' ');
-                string sortColumn2nd = parts2nd[0];
-                string sortDirection2nd = parts2nd[1];
-                if(sortDirection1st == "asc" && sortDirection2nd == "asc")
-                    companies = companies.OrderBy(c => c.GetType().GetProperty(sortColumn1st)?.GetValue(c, null) ??
-                            c.ContactDetails.GetType().GetProperty(sortColumn1st)?.GetValue(c, null))
-                        .ThenBy(c => c.GetType().GetProperty(sortColumn2nd)?.GetValue(c, null));
-                else if (sortDirection1st == "desc" && sortDirection2nd == "asc")
-                    companies = companies.OrderByDescending(c => c.GetType().GetProperty(sortColumn1st)?.GetValue(c, null) ??
-                            c.ContactDetails.GetType().GetProperty(sortColumn1st)?.GetValue(c, null))
-                        .ThenBy(c => c.GetType().GetProperty(sortColumn2nd)?.GetValue(c, null));
-                else if (sortDirection1st == "asc" && sortDirection2nd == "desc")
-                    companies = companies.OrderBy(c => c.GetType().GetProperty(sortColumn1st)?.GetValue(c, null) ??
-                            c.ContactDetails.GetType().GetProperty(sortColumn1st)?.GetValue(c, null))
-                        .ThenByDescending(c => c.GetType().GetProperty(sortColumn2nd)?.GetValue(c, null));
-                else
-                    companies = companies.OrderByDescending(c => c.GetType().GetProperty(sortColumn1st)?.GetValue(c, null) ??
-                            c.ContactDetails.GetType().GetProperty(sortColumn1st)?.GetValue(c, null))
-                        .ThenByDescending(c => c.GetType().GetProperty(sortColumn2nd)?.GetValue(c, null));
-            }
+
+            if (sortCriteria.Length > 0)
+                companies = SortFunction(companies, sortCriteria);
 
             return Json(companies);
         }
 
         [HttpGet("ExportData")]
-        public FileStreamResult ExportData()
+        public FileStreamResult ExportData([FromQuery] string sortCriteria)
         {
-            List<Company> companies = _memoryStorage.GetAll<Company>().ToList();
+            IEnumerable<CompanyViewModel> companies = _memoryStorage.GetAll<Company>().Select(company => new CompanyViewModel(company));
+            if (!string.IsNullOrWhiteSpace(sortCriteria))
+            {
+                string[] sortCriteriaArray = sortCriteria.Split(',');
+                companies = SortFunction(companies, sortCriteriaArray);
+            }
 
             // Create the memory stream and stream writer
             MemoryStream stream = new MemoryStream();
@@ -88,6 +57,53 @@ namespace ExerciseWebApp.Controllers
 
             // Return the result
             return result;
+        }
+
+        // For more dynamic sorting i would use a nuget package called System.Linq.Dynamic.Core
+        // But for the purpose of this exercise i will give the posibility to sort by 2 columns only
+        [NonAction]
+        private IEnumerable<CompanyViewModel> SortFunction(IEnumerable<CompanyViewModel> companies, string[] sortCriteria)
+        {
+            string[] parts1st = sortCriteria[0].Split(' ');
+            string sortColumn1st = parts1st[0];
+            string sortDirection1st = parts1st[1];
+            if (sortCriteria.Length == 1)
+            {
+                companies = sortDirection1st == "asc" ?
+                    companies.OrderBy(c => GetPropertyValue(c, sortColumn1st)) :
+                    companies.OrderByDescending(c => GetPropertyValue(c, sortColumn1st));
+            }
+            if (sortCriteria.Length == 2)
+            {
+                string[] parts2nd = sortCriteria[1].Split(' ');
+                string sortColumn2nd = parts2nd[0];
+                string sortDirection2nd = parts2nd[1];
+
+                if (sortDirection1st == "asc" && sortDirection2nd == "asc")
+                    companies = companies.OrderBy(c => GetPropertyValue(c, sortColumn1st))
+                        .ThenBy(c => GetPropertyValue(c, sortColumn2nd));
+
+                else if (sortDirection1st == "desc" && sortDirection2nd == "asc")
+                    companies = companies.OrderByDescending(c => GetPropertyValue(c, sortColumn1st))
+                        .ThenBy(c => GetPropertyValue(c, sortColumn2nd));
+
+                else if (sortDirection1st == "asc" && sortDirection2nd == "desc")
+                    companies = companies.OrderBy(c => GetPropertyValue(c, sortColumn1st))
+                        .ThenByDescending(c => GetPropertyValue(c, sortColumn2nd));
+
+                else
+                    companies = companies.OrderByDescending(c => GetPropertyValue(c, sortColumn1st))
+                        .ThenByDescending(c => GetPropertyValue(c, sortColumn2nd));
+            }
+            return companies;
+        }
+        [NonAction]
+        private object? GetPropertyValue(CompanyViewModel obj, string propertyName)
+        {
+            PropertyInfo propertyInfo = typeof(Company).GetProperty(propertyName) ?? typeof(Contact).GetProperty(propertyName);
+            if (propertyInfo == null) return null;
+
+            return propertyInfo.DeclaringType == typeof(Company) ? propertyInfo.GetValue(obj) : propertyInfo.GetValue(obj.ContactDetails);
         }
     }
 }
--- ExerciseWebApp/wwwroot/js/viewData.js
+++ ExerciseWebApp/wwwroot/js/viewData.js
@@ -1,45 +1,106 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
+    // Reset saved prefs on page refresh
+    localStorage.setItem('sortPrefs', JSON.stringify([]));
     // Add event listeners for click events on the table header cells
     const headerCells = document.querySelectorAll('th');
-    headerCells.forEach(function (cell) {
-        cell.addEventListener('click', function (event) {
-            // Get the name of the property to sort by
+    headerCells.forEach( (cell) => {
+        cell.addEventListener('click', (event) => {
+
             const sortProperty = event.target.getAttribute('data-sort');
+            // Get the current sortPreference
+            let sortPreference = event.target.getAttribute('data-sort-preference');
 
-            // Get the current sorting preference
-            let sortPreference = event.target.getAttribute('data-sort-preference') || 'unsorted';
+            const oldCellCriteria = `${sortProperty} ${sortPreference}`;
 
-            // Set the new sorting preference
-            event.target.classList = [];
-            if (sortPreference === 'asc') {
-                event.target.setAttribute('data-sort-preference', 'desc');
-                event.target.classList.add('desc');
-            } else if (sortPreference === 'desc') {
-                event.target.setAttribute('data-sort-preference', 'unsorted');
-                event.target.classList.add('unsorted');
-            } else {
-                event.target.setAttribute('data-sort-preference', 'asc');
-                event.target.classList.add('asc');
-            }
-            sortPreference = event.target.getAttribute('data-sort-preference');
+            const savedPrefs = getPreferences();
+            let filteredPrefs = savedPrefs.filter((data) => {
+                if (data !== oldCellCriteria) return data;
+            });
 
-            const sortCriteria = [`${sortProperty} ${sortPreference}`];
+            // Get next sortPreference
+            sortPreference = getNextSort(sortPreference);
+
+            
+            let sortCriteriaArr = [];
+            if (sortPreference !== 'unsorted')
+                sortCriteriaArr = [...filteredPrefs, `${sortProperty} ${sortPreference}`];
+            else
+                sortCriteriaArr = [...filteredPrefs];
+
+            // Keep only last 2 sorting prefs
+            if (sortCriteriaArr.length === 3)
+                sortCriteriaArr = sortCriteriaArr.splice(1, 2);
+
+            // Save Preferences
+            setPreferences(sortCriteriaArr);
+
+            // Update the headers sorting class
+            updateHeadersUI(sortCriteriaArr);
 
             // Send an AJAX request to the server to retrieve the sorted data
             $.ajax({
                 type: 'POST',
                 url: '/SortData',
-                data: { sortCriteria: sortCriteria },
+                data: { sortCriteria: sortCriteriaArr },
                 success: function (data) {
                     updateTable(data);
                 }
             });
         });
     });
+    document.querySelector('.export-button').addEventListener('click', () => {
+        const sortingPreferences = getPreferences();
+        // Construct the query string for the sorting preferences
+        const queryString = `sortCriteria=${sortingPreferences.join(',')}`;
+        // Send the request to the server with the sorting preferences in the query string
+        const url = `/ExportData?${queryString}`;
+        window.location = url;
+    });
 });
 
+function updateHeadersUI(sortCriteriaArr) {
+    const headerCells = document.querySelectorAll('th');
+    headerCells.forEach((cell) => {
+        const sortProperty = cell.getAttribute('data-sort');
+
+        cell.classList = [];
+        const hasPref = sortCriteriaArr.find((data) => {
+            return data.split(' ')[0] === sortProperty;
+        });
+
+        if (hasPref) {
+            const sortPreference = hasPref.split(' ')[1];
+            cell.setAttribute('data-sort-preference', sortPreference);
+            cell.classList.add(sortPreference);
+        } else {
+            cell.setAttribute('data-sort-preference', 'unsorted');
+            cell.classList.add('unsorted');
+        }
+    });
+}
+
+function getNextSort(sortPreference) {
+    if (sortPreference === 'asc') return 'desc';
+    else if (sortPreference === 'desc') return 'unsorted';
+    else return 'asc';
+}
+
+function setPreferences(sortCriteria) {
+    const localSortPreference = JSON.stringify(sortCriteria);
+    localStorage.setItem('sortPrefs', localSortPreference);
+}
+
+function getPreferences() {
+    const localSortPreference = localStorage.getItem('sortPrefs');
+    let sortPrefsArray = [];
+    if (localSortPreference) {
+        sortPrefsArray = JSON.parse(localSortPreference);
+    }
+    return sortPrefsArray;  
+}
+
 function updateTable(data) {
-    // Clear thecurrent data from the table
+    // Clear the current data from the table
     let tableBody = document.querySelector('tbody');
     tableBody.innerHTML = '';
     // Populate the table with the new data
